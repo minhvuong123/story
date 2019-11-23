739756e366af103146e4dee4c12a5eeee3b99b37
minhvuong123 <minhvuong12222@gmail.com>
2019-11-23 14:36:39 +0700

    handle mouse event play

--- client/src/components/playDetail/index.js
+++ client/src/components/playDetail/index.js
@@ -4,12 +4,16 @@ import moment from 'moment';
 class PlayDetail extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      statusProgress: false,
+      abc: 0,
+    }
     this.refProgress = React.createRef();
   }
   
 
   componentDidMount(){
-    this.refProgress.current.addEventListener('click', this.handleProgress)
+    this.refProgress.current.addEventListener('click', this.handleProgress);
   }
 
   handleProgress = (e) => {
@@ -19,12 +23,35 @@ class PlayDetail extends Component {
     currentTimeAudio(progress);
   }
 
+  startSetProgress = (e) => {
+    this.setState({
+      statusProgress: true
+    })
+    this.setProgress(e);
+  }
+
+  setProgress = (e) => {
+    const { statusProgress } = this.state;
+    const { player, duration, currentTimeAudio } = this.props;
+    if(statusProgress){
+      const progress = (e.nativeEvent.offsetX / this.refProgress.current.getBoundingClientRect().width) * duration;
+      player.current.currentTime = progress;
+      currentTimeAudio(progress);
+    }
+  }
+
+  stopSetProgress = (e) => {
+    this.setState({
+      statusProgress: false
+    })
+    this.setProgress(e)
+  }
+
   render() {
-    const { duration, currentTime } = this.props;    
+    const { duration, currentTime } = this.props;   
     const parseCurrentTime = moment.utc(moment.duration(currentTime,"seconds").asMilliseconds()).format("HH:mm:ss");
     const parseDuration = moment.utc(moment.duration(duration,"seconds").asMilliseconds()).format("HH:mm:ss");
     const progressLength = currentTime / duration * 100 + '%';
-    
     return (
       <div className="col-7 col-sm-6 pl-0 pr-0">
         <div className="media-play-detail">
@@ -42,9 +69,15 @@ class PlayDetail extends Component {
                 <span className="time-duration">{parseDuration}</span>
               </div>
             </div>
-            <div className="song-player-time" ref={this.refProgress}>
+            <div 
+              className="song-player-time" 
+              ref={this.refProgress}
+              onMouseDown={this.startSetProgress}
+              onMouseMove={this.setProgress}
+              onMouseUp={this.stopSetProgress}
+              onMouseLeave={this.stopSetProgress}
+              >
               <div className="player-time-fill" style={{width: progressLength}} />
-              <div className="player-time-buffer" style={{left: progressLength}} />
             </div>
           </div>
         </div>
